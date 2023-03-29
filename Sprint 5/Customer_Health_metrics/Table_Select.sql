2f37e91d3a0a3d3886036d00c607409ab3f6c970
wz2392 <99241150+wz2392@users.noreply.github.com>
2023-03-29 19:02:18 -0400

    Update Table_Select.sql

--- Sprint 5/Customer_Health_metrics/Table_Select.sql
+++ Sprint 5/Customer_Health_metrics/Table_Select.sql
@@ -1,4 +1,10 @@
 SELECT *, lateShipment_30_rate/100 AS LateShipmetnRate_30, validTracking_rate_30/100 AS VaildTrackingRate_30,
 preFulfillmentCancellation_7_rate/100 AS preFulfillment_7rate
-FROM `bigqueryexport-183608.amazon.customer_health_metrics` 
-WHERE DATE(_PARTITIONTIME) = '2023-03-27'
+FROM `bigqueryexport-183608.amazon.customer_health_metrics`
+inner join
+(
+SELECT max(snapshot_date) AS LatestDate, mp_sup_key AS suppliers
+  FROM`bigqueryexport-183608.amazon.customer_health_metrics`
+  GROUP BY mp_sup_key) AS submax
+  on snapshot_date = submax.LatestDate AND mp_sup_key = submax.suppliers
+
