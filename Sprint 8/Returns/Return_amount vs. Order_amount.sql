eba807f4b8a573cf8ff0bb7085b5693b065a2823
wz2392 <99241150+wz2392@users.noreply.github.com>
2023-04-18 00:24:03 -0400

    Update Return_amount vs. Order_amount.sql

--- Sprint 8/Returns/Return_amount vs. Order_amount.sql
+++ Sprint 8/Returns/Return_amount vs. Order_amount.sql
@@ -6,3 +6,16 @@ FROM`bigqueryexport-183608.amazon.returns`
 WHERE DATE(order_date) BETWEEN '2022-01-01' AND CURRENT_DATE('America/Indiana/Indianapolis')
   AND return_request_status = "Approved" 
 GROUP BY order_id, mp_sup_key,order_date
+
+### Returns by Categories
+SELECT R.order_id,R.mp_sup_key,R.order_date,R.a_to_z_claim,R.in_policy,R.is_prime,IFNULL(P.product_group,"Unknown") AS Product_Group
+FROM
+(SELECT order_id,asin, mp_sup_key,order_date,a_to_z_claim,in_policy,is_prime
+FROM`bigqueryexport-183608.amazon.returns` 
+WHERE DATE(order_date) BETWEEN '2022-01-01' AND CURRENT_DATE('America/Indiana/Indianapolis')
+  AND return_request_status = "Approved" ) AS R
+LEFT JOIN
+(SELECT id,product_group
+FROM `bigqueryexport-183608.amazon.products` 
+WHERE id_type = "ASIN") AS P
+ON R.asin = P.id
